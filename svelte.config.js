82e6ef0ef6e7b28c56143291dbb0a528ee8afb7a
Dayanandh Jangamsrinivas <dayanandhjangam@gmail.com>
2025-03-24 00:16:57 -0400

    added nojekyll

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+const dev = process.env.NODE_ENV === 'development';
 const config = {
   preprocess: vitePreprocess(),
 
@@ -14,7 +15,7 @@ const config = {
       }
     ),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/sri-balaji-suppliers' : ''
+      base: dev ? '' : '/sri-balaji-suppliers'
     },
     prerender: {
       handleMissingId: 'ignore'
